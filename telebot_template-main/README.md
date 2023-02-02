84372ce8479bc840614063e46ab0ce07acf973d2
Ulug'bek Xudoyqulov <37562757+UlugbekUK@users.noreply.github.com>
2023-02-02 10:30:38 +0500

    Added new files

---
+++ telebot_template-main/README.md
@@ -0,0 +1,3 @@
+
+
+ Note that this template requires pyTelegramBotAPI version higher or equal to 4.4.0.
---
+++ telebot_template-main/tgbot/config.py
@@ -0,0 +1,3 @@
+# any configuration should be stored here
+
+TOKEN = '5377491589:AAF02ZZ0sJfIY2FtjLBn59S_lKei7Hg__xM' # configure env if you need;
